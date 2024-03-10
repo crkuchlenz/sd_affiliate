b6459b292c1cf2b682e6069ccff844a9683007e3
Charley Kuchlenz <crkuchlenz@gmail.com>
2024-03-10 10:13:56 -0500

    merge to master

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Charley Kuchlenz",
+        "title": "Blog Post 1",
+        "content": "Blog post content",
+        "date_posted": "March 8, 2024"
+    },
+    {
+        "author": "Charley Kuchlenz",
+        "title": "Blog Post 2",
+        "content": "Blog post content 2",
+        "date_posted": "March 8, 2024"
+    },
+    {
+        "author": "Charley Kuchlenz",
+        "title": "Blog Post 3",
+        "content": "Blog post content 3",
+        "date_posted": "March 8, 2024"
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        "posts": posts
+    }
+    return HttpResponse(render(request, "blog/home.html", context))
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return HttpResponse(render(request, "blog/about.html", {"title":"About"}))
--- sd_affiliate/urls.py
+++ sd_affiliate/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("", include("base_site.urls")),
+    path("", include("blog.urls")),
     path("admin/", admin.site.urls),
-    path("blog/", include("blog.urls")),
 ]
